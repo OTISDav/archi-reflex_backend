70b77ee6b7aea63c9bee8004e3bb67cff9401bc5
OTISDav <159715854+OTISDav@users.noreply.github.com>
2025-12-31 08:03:31 +0000

    ajout amin.py pour adminPannel

--- projects/admin.py
+++ projects/admin.py
@@ -1,3 +1,48 @@
 from django.contrib import admin
+from .models import Project
 
-# Register your models here.
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = (
+        'title',
+        'year',
+        'created_at',
+        'is_published',
+    )
+
+    list_filter = (
+        'year',
+        'is_published',
+    )
+
+    search_fields = (
+        'title',
+        'description',
+    )
+
+    ordering = ('-year',)
+
+    readonly_fields = ('created_at', 'updated_at')
+
+    fieldsets = (
+        ('Informations du projet', {
+            'fields': (
+                'title',
+                'description',
+                'year',
+                'image',
+            )
+        }),
+        ('Publication', {
+            'fields': (
+                'is_published',
+            )
+        }),
+        ('Dates', {
+            'fields': (
+                'created_at',
+                'updated_at',
+            )
+        }),
+    )
