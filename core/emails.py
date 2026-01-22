be9c17256fb7b81a805112f4578b6863df1df82a
OTISDav <159715854+OTISDav@users.noreply.github.com>
2026-01-22 13:31:57 +0000

    logique metir email pour rdv

--- core/emails.py
+++ core/emails.py
@@ -4,12 +4,13 @@ import requests
 DEFAULT_SENDER = {"name": "OTISDav", "email": "david.botcholi@davidbotcholi.online"}
 ADMIN_EMAIL = 'davidbotcholi2003@gmail.com'
 
-def send_notification(subject, message, recipient):
-
+def send_notification(subject, message, recipient, **kwargs):
+    """
+    kwargs peut contenir d'autres informations si nécessaire
+    """
     BREVO_API_KEY = os.environ.get('BREVO_API_KEY')
-
     if not BREVO_API_KEY:
-        print("Clé API Brevo introuvable ! Vérifie que la variable d'environnement est bien définie sur Render et que le service est redéployé.")
+        print("Clé API Brevo introuvable !")
         return {"error": "Clé API introuvable"}
 
     url = "https://api.brevo.com/v3/smtp/email"
@@ -18,6 +19,7 @@ def send_notification(subject, message, recipient):
         "api-key": BREVO_API_KEY,
         "content-type": "application/json"
     }
+
     data = {
         "sender": DEFAULT_SENDER,
         "to": [{"email": recipient}],
@@ -27,13 +29,9 @@ def send_notification(subject, message, recipient):
 
     try:
         response = requests.post(url, json=data, headers=headers, timeout=10)
-        if response.status_code not in (200, 201):
-            print(f"Erreur envoi email à {recipient}: {response.text}")
-            return {"error": response.text, "status_code": response.status_code}
-        else:
-            print(f"Email envoyé à {recipient}: {subject}")
-            return response.json()
+        response.raise_for_status()
+        print(f"Email envoyé à {recipient}: {subject}")
+        return response.json()
     except requests.RequestException as e:
-        print(f"Exception envoi email à {recipient}: {e}")
+        print(f"Erreur envoi email à {recipient}: {e}")
         return {"error": str(e)}
-
