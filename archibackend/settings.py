c45d3d20749799065f76015fb67b738d389601c1
OTISDav <159715854+OTISDav@users.noreply.github.com>
2026-01-23 07:05:21 +0000

    logique metir email pour rdv

--- archibackend/settings.py
+++ archibackend/settings.py
@@ -143,14 +143,14 @@ REST_FRAMEWORK = {
 }
 
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'ddavidotis@gmail.com'
-EMAIL_HOST_PASSWORD = 'mxbx koce zrro gqsr'
-DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
-ADMIN_EMAIL = 'davidbotcholi2003@gmail.com'
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_PORT = 587
+# EMAIL_USE_TLS = True
+# EMAIL_HOST_USER = 'ddavidotis@gmail.com'
+# EMAIL_HOST_PASSWORD = 'mxbx koce zrro gqsr'
+# DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+# ADMIN_EMAIL = 'davidbotcholi2003@gmail.com'
 
 
 
